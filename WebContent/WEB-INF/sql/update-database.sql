b174f7860077193ef52bdd09392e946224295fca
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-03-16 10:11:19 -0300

    Primer Commit desde eclipse

---
+++ WebContent/WEB-INF/sql/update-database.sql
@@ -0,0 +1,20 @@
+drop table if exists bsframework.tSessionData;
+drop table if exists bsframework.tSession;
+
+create table bsframework.tSession (
+	cId		BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	cSessionId 	VARCHAR(32) NOT NULL UNIQUE,
+	cLastAccess	TIMESTAMP NOT NULL
+) ENGINE=innoDB;
+
+create table bsframework.tSessionData (
+	cId		BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	cSession 	BIGINT  NOT NULL,
+	cName		VARCHAR(250) NOT NULL,
+	cData		TEXT
+) ENGINE=innoDB;
+
+
+ALTER TABLE bsframework.tSessionData
+ADD INDEX Index_Session (cSession ASC),
+ADD CONSTRAINT SessionData_To_Session FOREIGN KEY (cSession) REFERENCES tSession(cId);
